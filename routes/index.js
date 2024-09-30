1c6539218b6dbec004842acd21abd51b3558cbc8
Gift <aghaulor.gift@gmail.com>
2024-09-30 12:26:36 -0700

    Add user route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 // routes/index.js
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New route for creating a user
+router.post('/users', UsersController.postNew);
 
 export default router;

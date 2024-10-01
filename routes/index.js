ccfbe259bdfe6c09b4954fe20dbf4490a2541b8b
Gift <aghaulor.gift@gmail.com>
2024-10-01 03:49:09 -0700

    Add router to connect,disconnect and for the user

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = Router();
 
@@ -10,5 +11,10 @@ router.get('/stats', AppController.getStats);
 
 // New route for creating a user
 router.post('/users', UsersController.postNew);
+
+// New authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;

9ba0ac4ea6585ac4cf910b03896e141f4235687d
Gift <aghaulor.gift@gmail.com>
2024-10-02 11:33:50 -0700

    Update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,5 +1,6 @@
-const redisClient = require('../utils/redis');
-const dbClient = require('../utils/db');
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
 
 class AppController {
   static async getStatus(req, res) {
@@ -15,5 +16,4 @@ class AppController {
   }
 }
 
-module.exports = AppController;
-
+export default AppController;

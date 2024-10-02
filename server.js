ed0b51f01a238823f1ab4477ef63cb1d88991ecb
Gift <aghaulor.gift@gmail.com>
2024-10-02 11:11:29 -0700

    remove unused import

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 // server.js
 import express from 'express';
-import { redisClient } from './utils/redis';
-import dbClient from './utils/db';
 import routes from './routes/index';
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+export default app;

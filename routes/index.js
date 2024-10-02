3d3a51be3a9f74ab03bb8084518f1c450e868089
Gift <aghaulor.gift@gmail.com>
2024-10-02 10:21:32 -0700

    Add routes for each class

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,26 @@
+//Router/index
 const express = require('express');
-const router = express.Router();
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
 const FilesController = require('../controllers/FilesController');
 
-// AppController Routes
+const router = express.Router();
+
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// UsersController Routes
 router.post('/users', UsersController.postNew);
 router.get('/users/me', UsersController.getMe);
 
-// AuthController Routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 
-// FilesController Routes
 router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+router.get('/files/:id/data', FilesController.getFile);
 
 module.exports = router;

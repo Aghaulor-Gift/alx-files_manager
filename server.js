135550f0d25cd555ce95042613cb6d554eb6472c
Gift <aghaulor.gift@gmail.com>
2024-10-02 10:12:58 -0700

    Add server

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // server.js
-const express = require('express');
-const routes = require('./routes');
+import express from 'express';
+import { redisClient } from './utils/redis';
+import dbClient from './utils/db';
+import routes from './routes/index';
+
 
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());
-app.use('/', routes);
+app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
